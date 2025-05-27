034aeac39abc055c06c76448e23bebf795befa52
khankesuraj1 <160396375+khankesuraj1@users.noreply.github.com>
2025-05-27 13:34:49 +0530

    Create BaseTest.java

---
+++ BaseTest.java
@@ -0,0 +1,24 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
